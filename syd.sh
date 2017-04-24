ec36d5b8046449b6d4aa8cb5be7c4c3b9a6a5885
Lee Ashley <leeashley.silva@gmail.com>
2017-04-24 01:26:54 -0300

    tentando deletar

--- syd.sh
+++ syd.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-	echo "Digite os argumentos após o nome do script, com um esapço entre eles"
-	echo "Ex: $0 nome-do-arquivo nome-da-alteração nome branch"
+	echo "Digite os argumentos após o nome do script, com um esapço entre eles."
+	echo "Ex: $0 nome-do-arquivo nome-da-alteração nome-branch 1/2"
+	echo "Após o nome do branch digite o número 1 para salvar login e senha do seu github, e 2 para não salvar."
 fi
 
-if [ "$1" != "" ] && [ "$2" != "" ] && [ "$3" != "" ]; then
-echo "funciona"
-git add $1
-git commit -m $2
-git push $3 master
+if [ "$1" != "" ] && [ "$2" != "" ] && [ "$3" != "" ] && [ "$4" == "1" ] ; then
+	git add $1
+	git commit -m $2
+	git push $3 master
+	git config --global credential.helper cache
 fi
 
-
+if [ "$1" != "" ] && [ "$2" != "" ] && [ "$3" != "" ] && [ "$4" == "2" ] ; then
+	git add $1
+	git commit -m $2
+	git push $3 master
+	echo "Sua senha não foi salva, como desejado."
+fi
