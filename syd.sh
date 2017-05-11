7380fe8589931e410cd39f0157dd686d93dc6813
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2017-05-11 00:48:20 -0300

    Update syd.sh

--- syd.sh
+++ syd.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-	echo "Digite os argumentos após o nome do script, com um esapço entre eles."
-	echo "Ex: $0 nome-do-arquivo nome-da-alteração nome-remoto(origin) 1/2"
-	echo "Após o nome do branch digite o número 1 para salvar login e senha do seu github, e 2 para não salvar."
-	echo "o branch padrão é o master"
+	echo "usage: syd Nome_Do_Commit"
 fi
 
-if [ "$1" != "" ] && [ "$2" != "" ] && [ "$3" != "" ] && [ "$4" == "1" ] ; then
-	git add $1
-	git commit -m $2
-	git push $3 master
+if [ "$1" != "" ] ; then
+	git add '*'
+	git commit -m $1
+	git push origin master
 	git config --global credential.helper cache
-fi
-
-if [ "$1" != "" ] && [ "$2" != "" ] && [ "$3" != "" ] && [ "$4" == "2" ] ; then
-	git add $1
-	git commit -m $2
-	git push $3 master
-	echo "Não precisou salvar a senha."
 fi
